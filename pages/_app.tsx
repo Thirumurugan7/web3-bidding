c04724ad5a658da056f037733f5207a6888e632d
Vairamuthu <mvairamuthu2003@gmail.com>
2023-07-22 12:08:12 +0530

    updated the header and landing dashboard

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-
+import "../styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+import type { AppProps } from "next/app";
+import { WagmiConfig } from "wagmi";
+import { chains, wagmiClient } from "../config/walletConfig";
+import { Web3Context } from "../config/Web3Context";
+import Web3 from "web3";
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <Web3Context.Provider value={Web3}>
+        <Component {...pageProps} />
+      </Web3Context.Provider>
+    </WagmiConfig>
+  );
 }
 
-export default MyApp
+export default MyApp;
